192fe28961517b5a2fbfd13f651bb4db6e6a05ff
Jamalissaka <88450988+Jamalissaka@users.noreply.github.com>
2024-05-09 18:06:42 +0000

    Implement auth, appwrite and sentry

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,7 +22,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-//import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import { signIn, signUp, getLoggedInUser } from "@/lib/actions/user.actions";
 //import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -49,28 +49,28 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create plaid token
 
       if (type === "sign-up") {
-        //  const userData = {
-        //    firstName: data.firstName!,
-        //    lastName: data.lastName!,
-        //    address1: data.address1!,
-        //    city: data.city!,
-        //    state: data.state!,
-        //    postalCode: data.postalCode!,
-        //    dateOfBirth: data.dateOfBirth!,
-        //    ssn: data.ssn!,
-        //    email: data.email,
-        //    password: data.password,
-        //  };
-        //   const newUser = await signUp(userData);
-        //    setUser(newUser);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
+        const newUser = await signUp(userData);
+        setUser(newUser);
       }
 
       if (type === "sign-in") {
-        // const response = await signIn({
-        //  email: data.email,
-        //   password: data.password,
-        //  });
-        //  if (response) router.push("/");
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
       }
     } catch (error) {
       console.log(error);
@@ -107,7 +107,7 @@ const AuthForm = ({ type }: { type: string }) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          {/* <PlaidLink user={user} variant="primary" /> */}
+          {/*ev <PlaidLink user={user} variant="primary" /> */}
         </div>
       ) : (
         <>
